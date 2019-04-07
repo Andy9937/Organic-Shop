814b33320d162249427504f8e52688036fdfd0df
Andy9937 <ojhaannada@gmail.com>
2019-04-08 04:02:02 +0530

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDWXv1EqHp6WQjmFGZe9DNLlRw6BnHLHhY",
+    authDomain: "oshop-f4371.firebaseapp.com",
+    databaseURL: "https://oshop-f4371.firebaseio.com",
+    projectId: "oshop-f4371",
+    storageBucket: "oshop-f4371.appspot.com",
+    messagingSenderId: "124521819726"
+    
+  }
+};
